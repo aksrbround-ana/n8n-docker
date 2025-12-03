20ceb7eb4195370b9a473c507067cbf7f7aeb80c
Acround <acround@gmail.com>
2025-12-03 17:24:00 +0100

    add chat id to event

---
+++ site/migrations/m251203_162057_add_chat_id_to_event.php
@@ -0,0 +1,22 @@
+<?php
+
+use yii\db\Migration;
+
+class m251203_162057_add_chat_id_to_event extends Migration
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function safeUp()
+    {
+        $this->addColumn('{{%event}}', 'chat_id', $this->bigInteger()->notNull()->defaultValue(0)->after('id'));
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function safeDown()
+    {
+        $this->dropColumn('{{%event}}', 'chat_id');
+    }
+}
