bb678788104a43c3ed18f3f7b854103b36ab3425
Acround <acround@gmail.com>
2025-12-15 12:07:14 +0100

    fix migration

--- site/migrations/m251215_105442_create_poreski_kalendar_table.php
+++ site/migrations/m251215_105442_create_poreski_kalendar_table.php
@@ -16,7 +16,7 @@ class m251215_105442_create_poreski_kalendar_table extends Migration
             'id' => $this->primaryKey(),
             'input_date' => $this->dateTime()->notNull(),
             'notification_date' => $this->dateTime()->notNull(),
-            'activity_type' => $this->string(64)->notNull(),
+            'activity_type' => $this->string(256)->notNull(),
             'activity_text' => $this->string(1024)->notNull(),
             'activity'=> $this->string(32)->notNull()->defaultValue(''),
         ]);
