a9e925eb1ddfd5fcb9faf39c877f82ee8e17352d
Acround <acround@gmail.com>
2026-01-08 11:30:55 +0100

    Fix

--- site/migrations/m251213_084823_create_document_step_table.php
+++ site/migrations/m251213_084823_create_document_step_table.php
@@ -2,6 +2,7 @@
 
 use yii\db\Migration;
 use app\models\DocumentStep;
+use app\models\Document;
 
 /**
  * Handles the creation of table `{{%document_step}}`.
@@ -20,9 +21,9 @@ class m251213_084823_create_document_step_table extends Migration
             'updated_at' => $this->datetime()->defaultExpression('CURRENT_TIMESTAMP'),
         ]);
         $this->createIndex('idx_document_step_name',DocumentStep::tableName(),'name', true);
-        $this->insert(DocumentStep::tableName(), ['name' => DocumentStep::STEP_UPLOADED, 'created_at' => date('Y-m-d H:i:s')]);
-        $this->insert(DocumentStep::tableName(), ['name' => DocumentStep::STEP_CHECKED, 'created_at' => date('Y-m-d H:i:s')]);
-        $this->insert(DocumentStep::tableName(), ['name' => DocumentStep::STEP_NEEDS_REVISION, 'created_at' => date('Y-m-d H:i:s')]);
+        $this->insert(DocumentStep::tableName(), ['name' => Document::STATUS_UPLOADED, 'created_at' => date('Y-m-d H:i:s')]);
+        $this->insert(DocumentStep::tableName(), ['name' => Document::STATUS_CHECKED, 'created_at' => date('Y-m-d H:i:s')]);
+        $this->insert(DocumentStep::tableName(), ['name' => Document::STATUS_NEEDS_REVISION, 'created_at' => date('Y-m-d H:i:s')]);
     }
 
     /**
