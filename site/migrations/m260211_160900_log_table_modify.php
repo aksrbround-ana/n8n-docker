66e6bbab07c9d2036ca7abdfd76595e498a1ba84
Acround <acround@gmail.com>
2026-02-11 18:07:53 +0100

    fix

--- site/migrations/m260211_160900_log_table_modify.php
+++ site/migrations/m260211_160900_log_table_modify.php
@@ -10,10 +10,12 @@ class m260211_160900_log_table_modify extends Migration
     public function safeUp()
     {
         $this->dropColumn('log', 'tg_id');
+        $this->dropColumn('log', 'message');
         $this->dropColumn('log', 'updated_at');
         $this->dropColumn('log', 'created_at');
-        $this->addColumn('log', 'ai_text', $this->text()->after('message'));
-        $this->addColumn('log', 'created_at', $this->integer()->after('ai_text'));
+        $this->addColumn('log', 'message', $this->json());
+        $this->addColumn('log', 'ai_text', $this->text());
+        $this->addColumn('log', 'created_at', $this->dateTime()->notNull()->defaultExpression('CURRENT_TIMESTAMP'));
     }
 
     /**
